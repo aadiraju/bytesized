383d61e68a9a08146e1eb628af224e8d7afd9f95
Monica Rampaul <monica.rampaul@alumni.ubc.ca>
2021-08-04 14:22:25 -0400

    All Q1 requirements done

--- routes/listorder.js
+++ routes/listorder.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const sql = require('mssql');
 const moment = require('moment');
 
+const priceFormat = (price) => {
+    return '$' + Number(price).toFixed(2);
+};
+
 router.get('/', function(req, res, next) {
     res.setHeader('Content-Type', 'text/html');
     //res.write('<title>YOUR NAME Grocery Order List</title>');
@@ -14,43 +18,33 @@ router.get('/', function(req, res, next) {
                         FROM ordersummary ordSum join customer on ordSum.customerId = customer.customerId`;
 
         let results = await pool.request().query(sqlQuery);
+        
         let ordList = [];
         
-        let prodInOrd = [];
-
-        for (let ord of results.recordset){
-            ordList.push(ord);
-        }
- 
-        //res.write("<table border = '1'><tr><th>Order Id</th><th>Order Date</th><th>Customer Id</th><th>Customer Name</th><th>Total Amount</th></tr>");
             for (let i = 0; i < results.recordset.length; i++) {
                 let result = results.recordset[i];
                 let ordId = result.orderId;
-                //res.write("<tr><td>" + result.orderId + "</td><td>" + result.ordDate + "</td><td>" + result.customerId + "</td><td>" + result.customerName + "</td><td> $" + result.totalAmount.toFixed(2) + "</td></tr><tr><td colspan = '5'>");
+                let prodInOrd = [];
 
                 let sqlQuery2 = `SELECT productId, quantity, price FROM orderproduct WHERE orderId = ` + ordId;
                 let results2 = await pool.request().query(sqlQuery2);
 
-                for (let prod of results2.recordset){
-                    prodInOrd.push(prod);
+                for (let j = 0; j < results2.recordset.length; j++){
+                    let result2 = results2.recordset[j];
+                    prodInOrd.push(result2);
                 }
-                //res.write("<table border = '2'><tr><th>Product Id</th><th>Quantity</th><th>Price</th></tr>");
-                //for (let j = 0; j < results2.recordset.length; j++){
-                    //let result2 = results2.recordset[j];
-                    //res.write("<tr><td>" + result2.productId + "</td><td>" + result2.quantity + "</td><td> $" + result2.price.toFixed(2) + "</td></tr>");
-                //}
-                //res.write("</table>");
-                //res.write("</td></tr>");
-            }
-            
-            //res.write("</table>");
-        return [ordList, prodInOrd];
 
-    })().then(([ordList, prodInOrd]) => {
+                ordList.push(result,prodInOrd)
+            }
+        return [ordList];
+
+    })().then(([ordList]) => {
         res.render('listorder', {
             title: 'Bytesized Order List',
             ordList: ordList,
-            prodInOrd: prodInOrd
+            helpers: {
+                priceFormat
+            }
         });
     }).catch(err =>{
         console.dir(err);
