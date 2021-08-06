1705b855d83caad3eed123535bde9e82eee5c0de
Jacob Vanzella <43144851+JacobVanzella@users.noreply.github.com>
2021-08-06 16:10:38 -0700

    Order Pull (#4)
    * First commit
    
    * Commit some stuff.
    
    * Mostly wurkun
    
    Timeout on checking order page after submitting order.
    
    * Mostly working improved visuals for show cart page.
    
    * Nice commit
    
    showcart and checkout pages are now looking good.
    
    TODO:
    Finish order.js
    Finish order.handlebars
    
    * general fixes
    
    * fix the timeout errors by closing resources
    
    * Fixed errors and finished order.js and most of the handlebars template
    
    * added empty cart messages and invalid user message
    
    * Added comments showing the error messages, completed order
    
    Co-authored-by: Abhineeth Adiraju <abhineeth00@gmail.com>

--- routes/checkout.js
+++ routes/checkout.js
@@ -3,16 +3,18 @@ const router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.setHeader('Content-Type', 'text/html');
-    res.write("<title>Grocery CheckOut Line</title>");
 
-    res.write("<h1>Enter your customer id to complete the transaction:</h1>");
-
-    res.write('<form method="get" action="order">');
-    res.write('<input type="text" name="customerId" size="50">');
-    res.write('<input type="submit" value="Submit"><input type="reset" value="Reset">');
-    res.write('</form>');
-
-    res.end();
+    (async function () {
+        return [];
+    })().then(() => {
+        res.render('checkout', {
+            title: 'Bytesized Checkout'
+        });
+    })
+        .catch(err => {
+            console.dir(err);
+            res.send(err);
+        });
 });
 
 module.exports = router;
