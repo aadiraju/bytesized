9f1f059eed1a05a852eda7ce0e8f02ff8ae54ce1
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-08-06 17:49:27 -0700

    revert back to cloud config

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ const app = express();
 
 // This DB Config is accessible globally
 dbConfig = {
-    user: 'SA',
-    password: 'YourStrong@Passw0rd',
-    server: 'db',
-    database: 'tempdb',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    server: '10.7.112.4',
+    database: process.env.DB_NAME,
+    port: 1433,
     options: {
         'enableArithAbort': true,
         'encrypt': false,
