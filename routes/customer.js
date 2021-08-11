f678a9e95084dcb3e48240adf3462afbc31957cc
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-08-10 19:50:19 -0700

    Customer Page (#12)
    * custinfo
    
    * Fixed customer info
    
    Co-authored-by: pavniagarwal <pavniagarwal>

--- routes/customer.js
+++ routes/customer.js
@@ -3,22 +3,54 @@ const router = express.Router();
 const sql = require('mssql');
 const auth = require('../auth');
 
-router.get('/', function(req, res, next) {
+function loginAuth(req, res, next) {
+    if (req.session.authenticatedUser)
+        next();
+    else {
+        //If Customer tries to access page without being logged in, send to login page with appropriate error message
+        req.session.loginMessage = "You haven't logged in yet.";
+        res.redirect("/login");
+    }
+}
 
-    res.setHeader('Content-Type', 'text/html');
+router.get('/', loginAuth, function (req, res, next) {
+    let userid = req.session.authenticatedUser;
 
-    // TODO: Print Customer information
+    (async function () {
+        let pool = await sql.connect(dbConfig);
+        let customerIDQuery = `SELECT customerId,
+                                      firstName,
+                                      lastName,
+                                      email,
+                                      phonenum,
+                                      address,
+                                      city,
+                                      state,
+                                      postalCode,
+                                      country,
+                                      userid
+                               FROM customer
+                               WHERE userid = @userid;`
+        let preppedSql = new sql.PreparedStatement(pool);
+        preppedSql.input('userid', sql.VarChar(20));
+        await preppedSql.prepare(customerIDQuery);
+        let customerResults = await preppedSql.execute({userid: userid});
+        let customer = customerResults.recordset[0];
 
-    (async function() {
-        try {
-
-	// TODO: Print customer info
-        } catch(err) {
+        pool.close();
+        return [customer]
+    })().then(([customer]) => {
+        res.render('customer', {
+            title: 'Bytesized Custmer Info',
+            username: req.session.authenticatedUser,
+            customer: customer,
+            active: {'customer': true}
+        });
+    })
+        .catch(err => {
             console.dir(err);
-            res.write(err + "")
-            res.end();
-        }
-    })();
+            res.send(err);
+        });
 });
 
 module.exports = router;
